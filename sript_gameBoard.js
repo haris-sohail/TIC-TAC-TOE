e62f2bc0e2be0e38a3ca11354598a11f8dcdfa94
haris-sohail <i210531@nu.edu.pk>
2023-08-03 19:02:27 +0500

    Added main menu, game board and ticks and crosses

--- sript_gameBoard.js
+++ sript_gameBoard.js
@@ -3,6 +3,7 @@ let turn = 'player 1';
 class Box {
     constructor(boxElValue) {
         this.boxEl = boxElValue;
+        this.empty = 1;
     }
 }
 
@@ -14,21 +15,57 @@ for (let i = 0; i < 9; i++) {
     boxes[i] = new Box(document.getElementsByClassName('box')[i]);
 }
 
-const drawCheck = (id) => {
-    console.log(id);
+const drawCross = (boxElValue) => {
+
+    checkImgEl = document.createElement('img');
+    checkImgEl.src = 'cross_mark.png';
+
+    checkImgEl.style.width = '68%';
+    checkImgEl.style.height = '68%';
+
+    boxElValue.style.transition = 'all 2s';
+
+
+    boxElValue.appendChild(checkImgEl);
+}
+
+const drawCheck = (boxElValue) => {
+
+    checkImgEl = document.createElement('img');
+    checkImgEl.src = 'check_mark.png';
+
+    checkImgEl.style.width = '80%';
+    checkImgEl.style.height = '80%';
+
+    boxElValue.style.transition = 'all 2s';
+
+
+    boxElValue.appendChild(checkImgEl);
 }
 
 // now we add event listeners to all boxes
 
 boxes.forEach(element => {
     element.boxEl.addEventListener('click', () => {
+        
+        
 
         if (turn == 'player 1') {
-            drawCheck(element.boxEl.id);
+            if (element.empty == 1) {
+                element.empty = 0;
+                drawCheck(element.boxEl);
+
+                turn = 'player 2';
+            }
         }
 
         else {
-            drawCross();
+            if (element.empty == 1) {
+                element.empty = 0;
+                drawCross(element.boxEl);
+
+                turn = 'player 1';
+            }
         }
 
     })
